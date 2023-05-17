f0ee2179453b3cec926b8807dfe1fefdcfa5741c
Aidan Castellanos <133064388+Aidannyboi@users.noreply.github.com>
2023-05-17 13:06:59 -0700

    Update Arrays_in_Loops.js
    Added descriptions to what the code does on lines 1,3,5,and 6.

--- Arrays_in_Loops.js
+++ Arrays_in_Loops.js
@@ -1,7 +1,7 @@
-var kids = ["Thomas","Cindy","Billy","Mandy"];
+var kids = ["Thomas","Cindy","Billy","Mandy"];              //Adds and array of all the kids names.
 
-var age = [8, 8, 9, 7];
+var age = [8, 8, 9, 7];                                     //Adds an array of all the set kids ages.
 
-for(var x = 0; x <= kids.length - 1; x++){
-console.log(kids[x] + " is now " + age[x] + " years old.")
+for(var x = 0; x <= kids.length - 1; x++){                  //loop for the legnth of the list and after every loop add 1 to X.
+console.log(kids[x] + " is now " + age[x] + " years old.")  //tells the log which kid is how old.
 }
